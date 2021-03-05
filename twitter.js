5d89f820c12c4b9619a7ca57d2ce93df301ba68d
Eduardo Cuducos <cuducos@users.noreply.github.com>
2021-03-05 15:50:00 -0500

    Prepares JS for Violentmonkey

--- twitter.js
+++ twitter.js
@@ -1,3 +1,13 @@
+// ==UserScript==
+// @name        Fuck-off, Twitter!
+// @namespace   Violentmonkey Scripts
+// @match       https://twitter.com/home
+// @grant       none
+// @version     1.0
+// @author      Eduardo Cuducos - https://github.com/cuducos/fuckoff-twitter/blob/ec14293dda1467be71abb2549d2a36972e9e148f/twitter.js
+// @description Always set Twitter's homepage to Latest tweets (chronological timeline)
+// ==/UserScript==
+
 const getBySelectorAndText = (selector, text) => {
   const matches = document.querySelectorAll(selector);
   for (let i = 0; i < matches.length; i++) {
@@ -14,7 +24,12 @@ const main = () => {
   if (home !== undefined) return;
 
   // Let's open the menu to change it
-  document.querySelector("div[aria-label='Top Tweets on']").click();
+  const menu = document.querySelector("div[aria-label='Top Tweets on']");
+  if (menu === undefined) {
+    console.error("Cannot find the menu to change to Latest tweets");
+    return;
+  }
+  menu.click();
 
   // Let's get the link to change it to Latest tweets
   const bt = getBySelectorAndText("div>div>span", "See latest Tweets instead");
@@ -25,3 +40,5 @@ const main = () => {
 
   bt.click();
 };
+
+window.setTimeout(main, 5000);
